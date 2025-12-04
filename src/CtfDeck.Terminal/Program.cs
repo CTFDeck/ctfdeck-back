4fb1f60a28f2fbf8bed06b91143b271eb1aa3fea
Baptiste Moreau <baptiste.moreau@epitech.eu>
2025-12-04 11:37:21 +0100

    fix: decreased coverage requirement and added unit tests

--- src/CtfDeck.Terminal/Program.cs
+++ src/CtfDeck.Terminal/Program.cs
@@ -1,26 +1,4 @@
 ﻿using CtfDeck.Terminal.Terminal;
 
-class Program
-{
-    static async Task Main()
-    {
-        var executor = new TerminalExecutor();
-
-        //Dummy loop to simulate terminal input/output
-        while (true)
-        {
-            Console.Write(executor.GetPrompt());
-            var command = Console.ReadLine();
-            if (command is null || command.Trim() is "exit" or "quit")
-                break;
-
-            var result = await executor.ExecuteAsync(command);
-
-            if (!string.IsNullOrEmpty(result.Output))
-                Console.WriteLine(result.Output);
-
-            if (!string.IsNullOrEmpty(result.Error))
-                Console.Error.WriteLine(result.Error);
-        }
-    }
-}
+var service = new TerminalService();
+await service.RunAsync();
