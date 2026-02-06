cb74fca544cf4e2586289bd82fc3d7cdea19db52
melvyn <melvyn@ngine.tv>
2026-02-06 02:20:46 +0100

    feat(CTFD-38): add missing folder script

---
+++ src/CtfDeck.Terminal/Script/Models/CustomScript.cs
@@ -0,0 +1,18 @@
+namespace CtfDeck.Terminal.Script.Models;
+
+public enum ScriptCategory
+{
+    Discovery = 0,
+    Web = 1,
+    ReverseShell = 2,
+    Exploit = 3,
+    Other = 4
+}
+
+public class CustomScript
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ScriptCategory Category { get; set; }
+    public string Template { get; set; } = string.Empty;
+}
